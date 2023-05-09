7a8fe51b70e14d22a640f3dc2f729fbb1a2275b7
Jean-EmmanuelP <Vous@exemple.com>
2023-05-09 22:36:12 +0200

    doing the dashboard layout

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -1,4 +1,6 @@
+import { fetchRedis } from "@/helpers/redis"
 import { authOptions } from "@/lib/auth"
+import { db } from "@/lib/db"
 import { addFriendValidator } from "@/lib/validations/add-friend"
 import { getServerSession } from "next-auth"
 
@@ -6,38 +8,33 @@ export async function POST(req: Request) {
     try {
         const body = await req.json()
 
-        const {email: emailToAdd} = addFriendValidator.parse(body.email)
-    
-        const RESTResponse = await fetch(`${process.env.URL}/get/user:email${emailToAdd}`, {
-            headers: {
-                Authorization: `Bearer ${process.env.TOKEN}`
-            },
-            cache: 'no-store',
+        const { email: emailToAdd } = addFriendValidator.parse(body.email)
+        console.log(`get id`, emailToAdd)
+
+        const idToAdd = await fetchRedis('get', `user:email:${emailToAdd}`) as string
+        if (!idToAdd) {
+            return new Response('This person does not exist.', {status: 400})
         }
-    )
+        
+        const session = await getServerSession(authOptions)
 
-    const data = await RESTResponse.json() as {result:string}
+        if (!session) {
+            return new Response('Unauthorized', {status: 401})
+        }
 
-    const idToAdd = data.result
-
-    if (!idToAdd) {
-        return new Response('This person does not exist.', {status: 400})
-    }
-    const session = await getServerSession(authOptions)
-
-    if (!session) {
-        return new Response('Unauthorized', {status: 401})
-    }
-
-    if (idToAdd === session.user.id)
-    {
-        return new Response('You cannot add yourself as friends', {status:400})
-    }
+        if (idToAdd === session.user.id)
+        {
+            return new Response('You cannot add yourself as friends', {status:400})
+        }
     
     // valid request
+    db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id)
 
-    console.log(data)
+    return new Response('OK')
+    // console.log(data)
     } catch (error) {
-
+        if (error instanceof z.ZodError) {
+            return new Response(`Invalid request`, {status: 400})
+        }
     }
 }
--- src/helpers/redis.ts
+++ src/helpers/redis.ts
@@ -1,4 +1,4 @@
-const upstashRedisRestUrl = process.env.NEXTAUTH_URL
+const upstashRedisRestUrl = process.env.URL
 const authToken = process.env.TOKEN
 
 type Command = 'zrange' | 'sismember' | 'get' | 'smembers'
@@ -8,4 +8,18 @@ export async function fetchRedis(
     ...args: (string | number)[]
 ) {
     const commandUrl = `${upstashRedisRestUrl}/${command}/${args.join('/')}`
+    
+    const response = await fetch(commandUrl, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+        cache: 'no-store',
+      })
+    
+      if (!response.ok) {
+        throw new Error(`Error executing Redis command: ${response.statusText}`)
+      }
+    
+      const data = await response.json()
+      return data.result
 }
