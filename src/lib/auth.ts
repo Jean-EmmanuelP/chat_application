e149aae5c1d1b3e493b6b7f2aa9096c7d020e02d
Jean-EmmanuelP <Vous@exemple.com>
2023-05-10 04:34:52 +0200

    done api/friend/(accept & deny) -> doing middleware login

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,17 @@
 import { NextAuthOptions } from "next-auth";
 import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
-import GoogleProvider from "next-auth/providers/google";
 import { db } from "./db";
+import GoogleProvider from "next-auth/providers/google";
+import { fetchRedis } from "@/helpers/redis";
 
-function getGooglCredentials() {
+function getGoogleCredentials() {
   const clientId = process.env.GOOGLE_CLIENT_ID;
   const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
 
   if (!clientId || clientId.length === 0) {
     throw new Error("Missing GOOGLE_CLIENT_ID");
   }
+
   if (!clientSecret || clientSecret.length === 0) {
     throw new Error("Missing GOOGLE_CLIENT_SECRET");
   }
@@ -22,38 +24,38 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
+
   pages: {
     signIn: "/login",
   },
   providers: [
     GoogleProvider({
-      clientId: getGooglCredentials().clientId,
-      clientSecret: getGooglCredentials().clientSecret,
+      clientId: getGoogleCredentials().clientId,
+      clientSecret: getGoogleCredentials().clientSecret,
     }),
   ],
   callbacks: {
     async jwt({ token, user }) {
-      if (user) {
-        const dbUserResult = (await fetchRedis("get", `user:${token.id}`)) as
-          | string
-          | null;
-        if (!dbUserResult) {
-          if (user) {
-            token.id = user!.id;
-          }
-          return token;
+      const dbUserResult = (await fetchRedis("get", `user:${token.id}`)) as
+        | string
+        | null;
+
+      if (!dbUserResult) {
+        if (user) {
+          token.id = user!.id;
         }
 
-        const dbUser = JSON.parse(dbUserResult) as User;
-
-        return {
-          id: dbUser.id,
-          name: dbUser.name,
-          email: dbUser.email,
-          image: dbUser.image,
-        };
+        return token;
       }
-      return token;
+
+      const dbUser = JSON.parse(dbUserResult) as User;
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      };
     },
     async session({ session, token }) {
       if (token) {
@@ -62,6 +64,7 @@ export const authOptions: NextAuthOptions = {
         session.user.email = token.email;
         session.user.image = token.picture;
       }
+
       return session;
     },
     redirect() {
