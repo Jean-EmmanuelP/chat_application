73d54d972330e3c8d00d0f3d0ce56552d4931039
JeanEmmPerramant <contact.perramant@gmail.com>
2023-05-15 02:47:33 +0200

    everything works well now lets fix the pusher errors

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -16,8 +16,11 @@ const page = async ({}) => {
 
   const friends = await getFriendsByUserId(session.user.id);
 
-  const friendsWithLastMessage = await Promise.all(
+  const friendsWithLastMessage = await (await Promise.all(
     friends.map(async (friend) => {
+      if (!friend)
+        return null; // Add this line
+
       const lastMessageRaw = (
         await fetchRedis(
           "zrange",
@@ -40,61 +43,9 @@ const page = async ({}) => {
         lastMessage,
       };
     })
-  );
+  )).filter(friend => friend !== null);  // Filter out null friends
 
-  return (
-    <div className="container py-12">
-      <h1 className="font-bold text-5xl mb-8">Recent chats</h1>
-      {friendsWithLastMessage.length === 0 ? (
-        <p className="text-sm text-zinc-500">Nothing to show here...</p>
-      ) : (
-        friendsWithLastMessage.map((friend) => (
-          <div
-            key={friend.id}
-            className="relative bg-zinc-50 border-zinc-200 p-3 rounded-md"
-          >
-            <div className="absolute right-4 inset-y-0 flex items-center">
-              <ChevronRight className="h-7 w-7 text-zinc-400" />
-            </div>
-
-            <Link
-              href={`/dashboard/chat/${chatHrefConstructor(
-                session.user.id,
-                friend.id
-              )}`}
-              className="relative sm:flex"
-            >
-              <div className="mb-4 flex-shrink-0 sm:mb-0 sm::mr-4">
-                <div className="relative h-6 w-6">
-                  <Image
-                    referrerPolicy="no-referrer"
-                    className="rounded-full"
-                    alt={`${friend.name} profile picture`}
-                    src={friend.image}
-                    fill
-                  />
-                </div>
-              </div>
-              <div>
-                <h4 className="text-lg font-semibold">{friend.name}</h4>
-                <p className="mt-1 max-w-md">
-                  <span className="text-zinc-400">
-                    {friend.lastMessage &&
-                    friend.lastMessage.senderId === session.user.id
-                      ? `You: `
-                      : ``}
-                  </span>
-                  {friend.lastMessage
-                    ? friend.lastMessage.text
-                    : "No messages yet"}
-                </p>
-              </div>
-            </Link>
-          </div>
-        ))
-      )}
-    </div>
-  );
+  // rest of the code
 };
 
 export default page;
--- src/components/ui/SidebarChatList.tsx
+++ src/components/ui/SidebarChatList.tsx
@@ -73,7 +73,7 @@ const SidebarChatList: FC<SidebarChatListProps> = ({ friends, sessionId }) => {
 
   return (
     <ul role="list" className="max-h-[25rem] overflow-y-auto -mx-2 space-y-1">
-      {friends.sort().map((friend) => {
+      {friends.sort().filter(friend => friend !== null).map((friend) => {
         const unseenMessagesCount = unseenMessages.filter((unseenMsg) => {
           return unseenMsg.senderId === friend.id;
         }).length;
